427a4609d5e8f25d0364aff1bfb6f90e7cee6f34
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-24 18:48:51 -0700

    Set up hyperapp

---
+++ index.js
@@ -0,0 +1,9 @@
+import { h, app } from "hyperapp";
+
+import view from "./src/js/components/View";
+import state from "./src/js/state/state";
+import actions from "./src/js/actions/actions";
+
+import "./src/css/main.scss";
+
+const main = app(state, actions, view, document.body);
