7acf4617aa682abdd4e2a1a7fcf5f63d66ae50f9
Ryan Kubik <rmkubik@mix.wvu.edu>
2018-07-24 18:51:27 -0700

    Basic input

---
+++ src/js/components/view.js
@@ -0,0 +1,10 @@
+import { h } from "hyperapp";
+
+const View = (state, actions) => (
+    <div>
+        <input type="text" />
+        <button>Submit</button>
+    </div>
+);
+
+export default View;
